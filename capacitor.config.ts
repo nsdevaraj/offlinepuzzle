78bd577d7e99eee885d61f4e848e270f4cb1b91c
N.S.Devaraj <nsdevaraj@gmail.com>
2026-04-17 11:45:00 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'devois.com.offlinepuzzle',
+  appName: 'OfflineChess',
+  webDir: 'build'
+};
+
+export default config;
