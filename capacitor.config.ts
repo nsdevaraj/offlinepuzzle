dcb4867525f83cd61ca988d852ddf82a715f8b0d
N.S.Devaraj <nsdevaraj@gmail.com>
2026-05-12 12:12:54 +0530

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.devois.offlinechess',
   appName: 'OfflineChess Pro',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
